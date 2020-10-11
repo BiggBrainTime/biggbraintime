ffb35ae85b0830d77442edfab15dc81e48bdc38c
Deep <deepmumbai16@gmail.com>
2020-10-12 01:05:31 +0530

    Restructured the project

--- bbt/settings.py
+++ bbt/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g-#hm2m!rp%8q4@9l9a3^d0c78d=2eqo^fpnc0h0mnz-=zzvht'
+SECRET_KEY = 'awz*jfk_+47lh_m%d5%*4lf-m00a19ri=8pzt-m)5$4-c=j1z%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
